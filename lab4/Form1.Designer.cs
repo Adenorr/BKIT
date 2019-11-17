181accebe1bb3c1204837e1cd86122f836ec90fc
Aletvans <aletvans@gmail.com>
2019-11-17 22:38:19 +0300

    changed lab4

--- lab4/Form1.Designer.cs
+++ lab4/Form1.Designer.cs
@@ -28,6 +28,7 @@
         /// </summary>
         private void InitializeComponent()
         {
+            this.components = new System.ComponentModel.Container();
             this.LoadButton = new System.Windows.Forms.Button();
             this.openFileDialog1 = new System.Windows.Forms.OpenFileDialog();
             this.SearchText = new System.Windows.Forms.TextBox();
@@ -35,6 +36,12 @@
             this.ShearchButton = new System.Windows.Forms.Button();
             this.aTimeWork = new System.Windows.Forms.Label();
             this.aFileName = new System.Windows.Forms.Label();
+            this.statusStrip1 = new System.Windows.Forms.StatusStrip();
+            this.toolStripStatusLabel1 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.toolStripStatusLabel2 = new System.Windows.Forms.ToolStripStatusLabel();
+            this.progressBar1 = new System.Windows.Forms.ProgressBar();
+            this.timer1 = new System.Windows.Forms.Timer(this.components);
+            this.statusStrip1.SuspendLayout();
             this.SuspendLayout();
             // 
             // LoadButton
@@ -80,26 +87,76 @@
             // 
             // aTimeWork
             // 
-            this.aTimeWork.Location = new System.Drawing.Point(620, 412);
+            this.aTimeWork.Location = new System.Drawing.Point(620, 396);
             this.aTimeWork.Name = "aTimeWork";
             this.aTimeWork.Size = new System.Drawing.Size(250, 17);
             this.aTimeWork.TabIndex = 4;
             this.aTimeWork.Text = "Время загрузки    мс";
             this.aTimeWork.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            this.aTimeWork.Visible = false;
             // 
             // aFileName
             // 
-            this.aFileName.Location = new System.Drawing.Point(12, 412);
+            this.aFileName.Location = new System.Drawing.Point(9, 396);
             this.aFileName.Name = "aFileName";
             this.aFileName.Size = new System.Drawing.Size(581, 23);
             this.aFileName.TabIndex = 5;
             this.aFileName.Text = "Открыт Файл                                                               ";
+            this.aFileName.Visible = false;
+            // 
+            // statusStrip1
+            // 
+            this.statusStrip1.ImageScalingSize = new System.Drawing.Size(20, 20);
+            this.statusStrip1.Items.AddRange(new System.Windows.Forms.ToolStripItem[] {
+            this.toolStripStatusLabel1,
+            this.toolStripStatusLabel2});
+            this.statusStrip1.Location = new System.Drawing.Point(0, 426);
+            this.statusStrip1.Name = "statusStrip1";
+            this.statusStrip1.Size = new System.Drawing.Size(882, 24);
+            this.statusStrip1.TabIndex = 6;
+            this.statusStrip1.Text = "statusStrip1";
+            // 
+            // toolStripStatusLabel1
+            // 
+            this.toolStripStatusLabel1.AutoSize = false;
+            this.toolStripStatusLabel1.BackColor = System.Drawing.SystemColors.ControlLight;
+            this.toolStripStatusLabel1.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatusLabel1.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripStatusLabel1.Name = "toolStripStatusLabel1";
+            this.toolStripStatusLabel1.Size = new System.Drawing.Size(450, 19);
+            this.toolStripStatusLabel1.Text = "Открыт файл.. ";
+            this.toolStripStatusLabel1.TextAlign = System.Drawing.ContentAlignment.MiddleLeft;
+            // 
+            // toolStripStatusLabel2
+            // 
+            this.toolStripStatusLabel2.AutoSize = false;
+            this.toolStripStatusLabel2.BackColor = System.Drawing.SystemColors.InactiveCaption;
+            this.toolStripStatusLabel2.DisplayStyle = System.Windows.Forms.ToolStripItemDisplayStyle.Text;
+            this.toolStripStatusLabel2.Font = new System.Drawing.Font("Segoe UI", 7.8F, System.Drawing.FontStyle.Regular, System.Drawing.GraphicsUnit.Point, ((byte)(204)));
+            this.toolStripStatusLabel2.Name = "toolStripStatusLabel2";
+            this.toolStripStatusLabel2.Size = new System.Drawing.Size(190, 19);
+            this.toolStripStatusLabel2.Text = "Время открытия файла ";
+            this.toolStripStatusLabel2.TextAlign = System.Drawing.ContentAlignment.MiddleRight;
+            // 
+            // progressBar1
+            // 
+            this.progressBar1.Location = new System.Drawing.Point(0, 396);
+            this.progressBar1.Name = "progressBar1";
+            this.progressBar1.Size = new System.Drawing.Size(882, 23);
+            this.progressBar1.TabIndex = 7;
+            // 
+            // timer1
+            // 
+            this.timer1.Interval = 5000;
+            this.timer1.Tick += new System.EventHandler(this.timer1_Tick);
             // 
             // Form1
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(8F, 16F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(882, 450);
+            this.Controls.Add(this.progressBar1);
+            this.Controls.Add(this.statusStrip1);
             this.Controls.Add(this.aFileName);
             this.Controls.Add(this.aTimeWork);
             this.Controls.Add(this.ShearchButton);
@@ -108,6 +165,8 @@
             this.Controls.Add(this.LoadButton);
             this.Name = "Form1";
             this.Text = "Подопригорова С. ИУ5-34Б";
+            this.statusStrip1.ResumeLayout(false);
+            this.statusStrip1.PerformLayout();
             this.ResumeLayout(false);
             this.PerformLayout();
 
@@ -122,6 +181,11 @@
         private System.Windows.Forms.Button ShearchButton;
         private System.Windows.Forms.Label aTimeWork;
         private System.Windows.Forms.Label aFileName;
+        private System.Windows.Forms.StatusStrip statusStrip1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel1;
+        private System.Windows.Forms.ToolStripStatusLabel toolStripStatusLabel2;
+        private System.Windows.Forms.ProgressBar progressBar1;
+        private System.Windows.Forms.Timer timer1;
     }
 }
 
--- lab4/Form1.cs
+++ lab4/Form1.cs
@@ -23,7 +23,7 @@ namespace WindowsFormsApp1
         private void LoadButton_Click(object sender, EventArgs e)
         {
             SearchText.Text = "";
-     
+            
             OpenFileDialog dialog = new OpenFileDialog();
             dialog.InitialDirectory = Application.CommonAppDataPath;
             dialog.Filter = "Text files|*.txt";
@@ -33,18 +33,31 @@ namespace WindowsFormsApp1
                 aFileName.Text = "Открываем файл.. " + dialog.FileName;
                 Stopwatch sw = new Stopwatch();
                 sw.Start();
+                progressBar1.Minimum = 0;
+                FileInfo fileInf = new FileInfo(dialog.FileName);
+                progressBar1.Maximum = Convert.ToInt32(fileInf.Length)*2;
+                progressBar1.Value = 0;
+                progressBar1.Visible = true;
+
                 foreach (string s in File.ReadAllText(dialog.FileName, Encoding.UTF8).Split(' ', '\t', '!', '?', ',', '-', '.', ':', '\r', '\n'))
                 {
                     if (!String.IsNullOrEmpty(s))
                     {
                         string lowercase = s.ToLower();
                         if (!words.Contains(lowercase)) words.Add(lowercase);
+                        progressBar1.Value = progressBar1.Value + Encoding.Unicode.GetBytes(lowercase).Length;
+                        
                     }
-                }             
+                }                
                 ShearchButton_Click(this, new EventArgs());
                 sw.Stop();
                 aFileName.Text = "Открыт файл.. " + dialog.FileName;
                 aTimeWork.Text = "Время открытия файла " + sw.ElapsedMilliseconds + " ms";
+                toolStripStatusLabel1.Text = "Открыт файл.. " + dialog.FileName;
+                toolStripStatusLabel2.Text = "Время открытия файла " + sw.ElapsedMilliseconds + " ms";
+                
+                timer1.Start(); 
+                
             }
         }
 
@@ -53,6 +66,10 @@ namespace WindowsFormsApp1
             if (words == null) return;
 
             Stopwatch sw = new Stopwatch();
+            progressBar1.Minimum = 0;
+            progressBar1.Maximum = words.Count+1;
+            progressBar1.Value = 0;
+
             sw.Start();
 
             ListData.BeginUpdate();
@@ -60,9 +77,12 @@ namespace WindowsFormsApp1
 
             string substring = SearchText.Text.ToLower();
             foreach (string s in words)
+            {
                 if (s.Contains(substring))
                     ListData.Items.Add(s);
-            ListData.EndUpdate();
+                progressBar1.Value++;  
+            }
+             ListData.EndUpdate();
 
             sw.Stop();
             aTimeWork.Text = "Время поиска " + sw.ElapsedMilliseconds + " мс";
@@ -72,6 +92,12 @@ namespace WindowsFormsApp1
         {
             if (e.Modifiers == Keys.None && (e.KeyCode == Keys.Enter || e.KeyCode == Keys.Return))
                 ShearchButton_Click(this, new EventArgs());
+        }
+
+        private void timer1_Tick(object sender, EventArgs e)
+        {
+            progressBar1.Visible = false;
+            timer1.Stop();
         }
     }
 }
